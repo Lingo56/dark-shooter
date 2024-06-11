c944cbf3ff6c18fd1fc499ed9da8d3f642a9faea
Peter Soava <thelingo56@gmail.com>
2024-06-11 01:31:41 -0700

    add combo drain timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     private int hitCount;
+    private Coroutine countdownCoroutine; // To store the reference to the running coroutine
 
     private void Awake()
     {
@@ -33,6 +34,32 @@ public class GameManager : MonoBehaviour
     {
         hitCount++;
         UIEvents.HitCountChanged(hitCount); // Trigger the UI event
+
+        // Stop the countdown coroutine if it's running
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+        }
+
+        // Start the countdown coroutine
+        countdownCoroutine = StartCoroutine(CountdownHitCount());
+    }
+
+    private IEnumerator CountdownHitCount()
+    {
+        yield return new WaitForSeconds(2f);
+
+        float comboReductionTimer = 0.7f;
+
+        while (hitCount > 0)
+        {
+            hitCount--;
+            UIEvents.HitCountChanged(hitCount); // Update the UI
+            yield return new WaitForSeconds(comboReductionTimer); // Adjust the countdown interval as needed
+            comboReductionTimer = comboReductionTimer / 1.4f;
+        }
+
+        countdownCoroutine = null; // Clear the coroutine reference when done
     }
 
     public int GetHitCount()
