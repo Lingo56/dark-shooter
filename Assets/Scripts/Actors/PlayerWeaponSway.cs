db5d8741a8a723a56d7275e7d5cf46986c5dbbd3
Peter Soava <thelingo56@gmail.com>
2024-06-21 23:28:18 -0700

    adjust weapon view model sway

--- Assets/Scripts/Actors/PlayerWeaponSway.cs
+++ Assets/Scripts/Actors/PlayerWeaponSway.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // TODO: Want sway to do translation as well as rotation. Currently only does rotation.
@@ -10,20 +8,21 @@ public class PlayerWeaponSway : MonoBehaviour
 {
     [Header("Sway Settings")]
     [SerializeField] private float _swayStep = 0.01f;
+
     [SerializeField] private float _maxSwayStepDistance = 0.06f;
     private Vector3 _swayPos;
 
     [Header("Sway Rotation Settings")]
-
     [SerializeField] private float _rotationStep = 4f;
+
     [SerializeField] private float _maxRotationStep = 5f;
     private Vector3 _swayEulerRot;
-
     [SerializeField] private float _smooth = 10f;
     private float _smoothRot = 12f;
 
     [Header("Bob Settings")]
     [SerializeField] private float _speedCurve;
+
     private float CurveSin { get => Mathf.Sin(_speedCurve); }
     private float CurveCos { get => Mathf.Cos(_speedCurve); }
 
@@ -35,6 +34,7 @@ public class PlayerWeaponSway : MonoBehaviour
 
     [Header("Bob Rotation Settings")]
     [SerializeField] private Vector3 _multiplier;
+
     private Vector3 _bobEulerRotation;
 
     private Vector2 _walkInput;
@@ -42,17 +42,18 @@ public class PlayerWeaponSway : MonoBehaviour
 
     private void Update()
     {
-        getInput();
+        GetInput();
 
-        sway();
-        swayRotation();
+        Sway();
+        SwayRotation();
         BobOffset();
         BobRotation();
 
         CompositePositionRotation();
     }
 
-    void getInput() {
+    private void GetInput()
+    {
         // Player direction
         _walkInput.x = Input.GetAxis("Horizontal");
         _walkInput.y = Input.GetAxis("Vertical");
@@ -63,7 +64,7 @@ public class PlayerWeaponSway : MonoBehaviour
         _lookInput.y = Input.GetAxis("Mouse Y");
     }
 
-    void sway()
+    private void Sway()
     {
         Vector3 invertLook = _lookInput * -_swayStep;
         invertLook.x = Mathf.Clamp(invertLook.x, -_maxSwayStepDistance, _maxSwayStepDistance);
@@ -72,14 +73,15 @@ public class PlayerWeaponSway : MonoBehaviour
         _swayPos = invertLook;
     }
 
-    private void swayRotation() {
+    private void SwayRotation()
+    {
         Vector2 invertLook = _lookInput * -_rotationStep;
         invertLook.x = Mathf.Clamp(invertLook.x, -_maxRotationStep, _maxRotationStep);
         invertLook.y = Mathf.Clamp(invertLook.y, -_maxRotationStep, _maxRotationStep);
         _swayEulerRot = new Vector3(invertLook.y, invertLook.x, invertLook.x);
     }
 
-    void BobOffset()
+    private void BobOffset()
     {
         _speedCurve += Time.deltaTime * ((Input.GetAxis("Horizontal") + Input.GetAxis("Vertical")) * _bobExaggeration) + 0.01f;
 
@@ -88,14 +90,14 @@ public class PlayerWeaponSway : MonoBehaviour
         _bobPosition.z = -(_walkInput.y * _travelLimit.z);
     }
 
-    void BobRotation()
+    private void BobRotation()
     {
         _bobEulerRotation.x = (_walkInput != Vector2.zero ? _multiplier.x * (Mathf.Sin(2 * _speedCurve)) : _multiplier.x * (Mathf.Sin(2 * _speedCurve) / 2));
         _bobEulerRotation.y = (_walkInput != Vector2.zero ? _multiplier.y * CurveCos : 0);
         _bobEulerRotation.z = (_walkInput != Vector2.zero ? _multiplier.z * CurveCos * _walkInput.x : 0);
     }
 
-    void CompositePositionRotation()
+    private void CompositePositionRotation()
     {
         transform.localPosition = Vector3.Lerp(transform.localPosition, _swayPos + _bobPosition, Time.deltaTime * _smooth);
         transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(_swayEulerRot) * Quaternion.Euler(_bobEulerRotation), Time.deltaTime * _smoothRot);
